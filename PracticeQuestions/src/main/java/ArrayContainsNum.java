7dfe16721270334a2a51a138c1df19985e813228
shubham <shubham@zipcodes-MBP-5.fios-router.home>
2021-01-19 21:11:18 -0500

    array contains num

--- PracticeQuestions/src/main/java/ArrayContainsNum.java
+++ PracticeQuestions/src/main/java/ArrayContainsNum.java
@@ -1,2 +1,22 @@
 public class ArrayContainsNum {
+
+    public static boolean arrayContainsNum(int[] arr, int num){
+
+        boolean containsNum = false;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == num ){
+                containsNum = true;
+                break;
+            } else {
+                containsNum = false;
+            }
+        } return containsNum;
+    }
+
+    public static void main(String[] args) {
+        int[] ar = {3, 4, 5, 8, 2, 1};
+        int n = 9;
+        System.out.println(arrayContainsNum(ar, n));
+    }
+
 }
