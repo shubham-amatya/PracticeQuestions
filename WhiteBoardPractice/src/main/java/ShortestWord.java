263b55ab10a2b69d8de5dff9fbfcf33c5482f3c7
shubham <shubham@zipcodes-MBP-5.fios-router.home>
2021-01-30 23:37:16 -0500

    input on shortest word

--- WhiteBoardPractice/src/main/java/ShortestWord.java
+++ WhiteBoardPractice/src/main/java/ShortestWord.java
@@ -1,2 +1,17 @@
 public class ShortestWord {
+    public static String findShortestWord(String sentence){
+        String[] words = sentence.split(" ");
+        String shortest = words[0];
+        for (int i = 0; i < words.length-1; i++) {
+            if(words[i].length()>words[i+i].length()){
+                shortest = words[i+1];
+            }
+        }return shortest;
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello I am Shu";
+        System.out.println(findShortestWord(s));
+    }
+
 }
