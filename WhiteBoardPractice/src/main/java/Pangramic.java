7cc5fa5ef40dd33fbac7151c14be20407b3ec6f7
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-02-16 12:27:01 -0500

    isPangramic function complete

--- WhiteBoardPractice/src/main/java/Pangramic.java
+++ WhiteBoardPractice/src/main/java/Pangramic.java
@@ -1,47 +1,54 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Pangramic {
     //pangramic is a sentence which has all the alphabets from a-z only once
 
-    public static boolean isPangramic(String str){
+    public static boolean isPangramic(String str) {
 
-        if(str.length()<26){
+        if (str.length() < 26) {
             return false;
         }
 
-        String withoutSpaces =  str.replaceAll("\\s", "");
+        String withoutSpaces = str.replaceAll("\\s", "");
         System.out.println(withoutSpaces);
         char[] arr = withoutSpaces.toCharArray();
-        String result = "";
 
 
-                Arrays.sort(arr);
+
+        Arrays.sort(arr);
         System.out.println(arr);
 
-        HashMap<Character , Integer> map = new HashMap<>();
-
-        for (int i = 0; i < map.size() ; i++) {
-            if(map.containsKey(arr[i])){
-                map.put(arr[i], map.get(arr[i])+1);
-            }else{
+        HashMap<Character, Integer> map = new HashMap<>();
+        String result = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
                 map.put(arr[i], 1);
             }
         }
-
-        for ( Character c: map.keySet()
-             ) {
-            result += c ;
+        char[] resultOfChar = new char[map.size()];
+        int j= 0;
+        for(Character i: map.keySet()){
+            resultOfChar[j] = i;
+            j++;
 
         }
+
+        for (int i = 0; i < resultOfChar.length; i++) {
+            result += resultOfChar[i];
+        }
+
+
         System.out.println(result);
 
 
+        String expected = "abcdefghijklmnopqrstuvwxyz";
 
-            String expected = "abcdefghijklmnopqrstuvwxyz";
-
-            if(expected.equals(result)){
-                return true;
+        if (expected.equals(result)) {
+            return true;
 
         }
         return false;
