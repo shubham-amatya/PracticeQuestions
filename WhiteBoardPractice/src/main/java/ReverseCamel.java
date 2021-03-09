cfcbaf1cdc4adabe4a561fc92d407aa4729f414c
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-09 10:57:34 -0500

    contd

--- WhiteBoardPractice/src/main/java/ReverseCamel.java
+++ WhiteBoardPractice/src/main/java/ReverseCamel.java
@@ -6,6 +6,20 @@ public class ReverseCamel {
      */
 
     public static String reverseCamel(String str){
+        String reversed = "";
 
+        for (int i = str.length()-1; i>0; i--) {
+            reversed += str.charAt(i);
+
+        }
+        String output = reversed.substring(0,1).toUpperCase()+reversed.substring(1);
+
+       return output;
+
+    }
+
+    public static void main(String[] args) {
+        String input = "the quick brown fox";
+        System.out.println(reverseCamel(input));
     }
 }
