46dad82b652e5d0aca5bd900c585c59c00348236
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-10 16:12:00 -0500

    contd

---
+++ WhiteBoardPractice/src/main/java/ReverseCasing.java
@@ -0,0 +1,15 @@
+public class ReverseCasing {
+
+    /**
+     * @param str a string input from user
+     * @return string with identical characters, each with opposite casing
+     */
+
+    public static String reverseCasing(String str){
+        char [] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+
+
+        }
+    }
+}
