7a5a72a67ccc7eb0134b486bf46f359dc6279991
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-10 21:29:35 -0500

    done

--- WhiteBoardPractice/src/main/java/ReverseCasing.java
+++ WhiteBoardPractice/src/main/java/ReverseCasing.java
@@ -7,9 +7,24 @@ public class ReverseCasing {
 
     public static String reverseCasing(String str){
         char [] arr = str.toCharArray();
+        String result = "";
         for (int i = 0; i < arr.length; i++) {
-
-
+            char c1 = arr[i];
+            if (Character.isUpperCase(c1)) {
+              arr[i] =  Character.toLowerCase(c1);
+            } else {
+                arr[i] = Character.toUpperCase(arr[i]);
+            }
         }
+            for (int j = 0; j < arr.length; j++) {
+                result += arr[j];
+            }
+
+            return result;
+    }
+
+    public static void main(String[] args) {
+        String input = "tHE quiCK brOwN";
+        System.out.println(reverseCasing(input));
     }
 }
