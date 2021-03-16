8f347241b3c35830c3a3a828eb4f9de3376a6ae1
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-15 22:44:45 -0400

    contd

--- WhiteBoardPractice/src/main/java/FirstChar.java
+++ WhiteBoardPractice/src/main/java/FirstChar.java
@@ -3,6 +3,8 @@ public class FirstChar {
     //Capitalize the first alphabets in a string
 
     public static String capitalizeFirstAlphabet(String sentence){
+        String [] arr = sentence.split(" ");
+
 
     }
 }
