605b1c2464163e666fec5aed3da9371cb7f2e649
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-15 22:51:32 -0400

    contd 2

--- WhiteBoardPractice/src/main/java/FirstChar.java
+++ WhiteBoardPractice/src/main/java/FirstChar.java
@@ -5,6 +5,26 @@ public class FirstChar {
     public static String capitalizeFirstAlphabet(String sentence){
         String [] arr = sentence.split(" ");
 
+        for (int i = 0; i < arr.length; i++) {
+            if(Character.isLowerCase(arr[i].charAt(0))){
+                Character.toUpperCase(arr[i].charAt(0));
+            }
+        }
+        String output = "";
+        for (int i = 0; i < arr.length; i++) {
+            output += arr[i];
+        }
 
+        return output;
     }
+
+    public static void main(String[] args) {
+        String s = "hello mister how do you do";
+        System.out.println();
+    }
+
+
+
+
+
 }
