d39721100ca9b41a813a31edbbe20bd9246fed61
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-17 01:12:00 -0400

    swap without variables

--- WhiteBoardPractice/src/main/java/NumSwap.java
+++ WhiteBoardPractice/src/main/java/NumSwap.java
@@ -12,9 +12,23 @@ public class NumSwap {
 
     }
 
+    public static void swapNumsWithoutVariable(int x, int y){
+        // The idea is to get a sum in one of the two given numbers.
+        // The numbers can then be swapped using the sum and subtraction from the sum.
+
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        System.out.println("x = " + x);
+        System.out.println("y = "+ y);
+
+
+    }
+
     public static void main(String[] args) {
         int x= 5, y = 10;
-        swapNumsUsingVariable(x, y);
+        swapNumsWithoutVariable(x, y);
 
     }
 
