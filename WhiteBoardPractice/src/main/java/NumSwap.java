2321772a4f3b7b7cca4ef232d8fd25860a82e7a7
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-17 01:03:53 -0400

    contd

--- WhiteBoardPractice/src/main/java/NumSwap.java
+++ WhiteBoardPractice/src/main/java/NumSwap.java
@@ -1,4 +1,21 @@
 public class NumSwap {
 
     //•	Swap 2 numbers, With and without a temp var
+
+    public static void swapNumsUsingVariable(int x, int y){
+        int z = x;
+        x = y;
+        y = z;
+
+        System.out.println("x = " + x);
+        System.out.println("y = "+ y);
+
+    }
+
+    public static void main(String[] args) {
+        int x= 5, y = 10;
+        swapNumsUsingVariable(x, y);
+
+    }
+
 }
