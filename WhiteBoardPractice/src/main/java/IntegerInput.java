dabce3676447d9222291f4285b1674ae99bda1ff
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-18 09:43:11 -0400

    int sum till n

---
+++ WhiteBoardPractice/src/main/java/IntegerInput.java
@@ -0,0 +1,13 @@
+public class IntegerInput {
+
+    /**
+     * @param n integer value input by client
+     * @return the sum of all integers between 0 and not including `n`
+     */
+
+    public static int sumTilli(int n){
+
+
+        return
+    }
+}
