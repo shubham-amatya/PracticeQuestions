ead869ddacd64110bef7720926c655f470356708
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-20 22:21:55 -0400

    contd

--- WhiteBoardPractice/src/main/java/UglyNumber.java
+++ WhiteBoardPractice/src/main/java/UglyNumber.java
@@ -1,5 +1,11 @@
 public class UglyNumber {
 
     //Write a Java program to check whether a given number is an ugly number.
+    //In number system, ugly numbers are positive numbers whose
+    // only prime factors are 2, 3 or 5.
 
+
+    public static boolean uglyNumberFinder(int num){
+
+    }
 }
