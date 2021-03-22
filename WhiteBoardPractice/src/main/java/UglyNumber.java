042b7cecc55b60be05d65a3234d74088c702ed71
shubham <shubham@MacBooks-MBP.fios-router.home>
2021-03-22 19:30:12 -0400

    in progress

--- WhiteBoardPractice/src/main/java/UglyNumber.java
+++ WhiteBoardPractice/src/main/java/UglyNumber.java
@@ -9,7 +9,8 @@ public class UglyNumber {
         if (num==1){
             return true;
         }
-        if((num%2==0 || num%3==0 || num %5==0) ){
+        if((num%7 !=0 || num%9!=0 || num%8!=0) && (num%2==0 || num%3==0 || num %5==0) ){
+
             return true;
         }
         return false;
